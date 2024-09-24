4823a15433865017d10e1a2b0c0b2a1ebb2b6e16
hatiaryaman <hatiaryaman@gmail.com>
2024-09-24 09:43:57 -0500

    Update player.js

--- player.js
+++ player.js
@@ -1,84 +1,141 @@
 const gravity = -9.81
 const airFriction = 0.99
 const friction = 0.94
-const dt = 0.2
-const maxSpeed = 30
-var jump = false
-var doubleTap = false
-
-class Player extends Object{
+var dt = 0.1
+const maxSpeed = 50
+class Player extends Obj{
     constructor(x, y, width, height, color) {
         super(x, y, width, height, color)
         this.velocity = new Vector(0, 0)
         this.acceleration = new Vector(0, gravity)
+
+        this.jump = false
+        this.airjump = true
+        this.doubleTap = false
+
+        // Hoi Hoi
+        this.ddash = 1;
+
+        this.w = width
+        this.h = height
+        this.dash = 0
     }
 
     move() {
-        // movements
+
         if (right || left) {
-            this.velocity.x += 2*(1 - this.velocity.x/maxSpeed)*right - 2*(1 + this.velocity.x/maxSpeed)*left
+            this.velocity.x += 2*(1 - this.velocity.x/(maxSpeed))*right - 2*(1 + this.velocity.x/(maxSpeed))*left
+        }
+        if(right + left > 0 && s == 1){
+            this.velocity.x = 200
+            this.velocity.x = (right == 1)? Math.abs(this.velocity.x): -1*Math.abs(this.velocity.x)
+            s = 0
         }
         if (right + left == 0) {
             this.velocity.x *= friction
         }
 
-        if (up) {
-            if (!doubleTap && this.velocity.y > 0) {
-                this.acceleration.y = gravity/2
-            } else {
-                this.acceleration.y = gravity
-            }
-            
-            if (jump) {
-                this.velocity.y = 30
-            }
-            jump = false
-        } else {
-            this.acceleration.y = gravity
-            doubleTap = true
+        if (Math.abs(this.velocity.x) < 0.000001) {
+            this.velocity.x = 0
         }
 
-        // storing collisions before movement
-        var bottom = this.collisionDown()
-        var top = this.collisionUp()
-        var l = this.collisionLeft()
-        var r = this.collisionRight()
+        this.width += ((this.w*(3 + (Math.abs(this.velocity.x)/maxSpeed))/(3 + (Math.abs(this.velocity.y)/maxSpeed))) - this.width)/4
+        
+        if (! down) {
+            this.height += ((this.h*(3 + (Math.abs(this.velocity.y)/maxSpeed))/(3 + (Math.abs(this.velocity.x)/maxSpeed))) - this.height)/4
+        }
+
+        if (up) {
+            if (!this.doubleTap && this.velocity.y > 0) {
+                this.acceleration.y = gravity/2
+            } else {
+                if (this.jump) {
+                    this.acceleration.y = gravity/2
+                } else {
+                    if (this.velocity.y > 0) {
+                        this.acceleration.y = gravity/2
+                    } else {
+                        this.acceleration.y = gravity
+                    }
+                }
+            }
+            
+            if (this.jump) {
+                this.velocity.y = 40
+            }
+
+            this.jump = false
+        } else {
+            this.acceleration.y = gravity
+            this.doubleTap = true
+        }
+
 
         // normal physics
         this.position = this.position.add(this.velocity.multiply(dt).add(this.acceleration.multiply(0.5*dt*dt)))
         this.velocity = this.velocity.add(this.acceleration.multiply(dt))
+        this.updateCoordinates()
 
-        // down collision
-        if (this.position.y - this.height/2 < bottom) {
-            this.position.y = bottom + this.height/2
-            this.velocity.y = 0
 
-            if (!up) {
-                jump = true
-                doubleTap = false
+        // collision
+        let count = 0;
+        for (let i = 0; i < ID; i++) {
+            if (this.id != i) {
+                if (this.collide(objects[i])) {
+                    count += 1
+                    let collideInfo = this.collisionMove(objects[i])
+
+                    this.position = this.position.add(collideInfo[0])
+
+                    if (collideInfo[1] == 1 || collideInfo[1] == 2) {
+                        this.velocity.y = 0
+                    }
+
+                    if (collideInfo[1] == 0) {
+                        this.velocity.x = 0
+                    }
+
+                    if (collideInfo[1] == 2) {
+                        if (!up) {
+                            this.jump = true
+                            this.doubleTap = false
+                            this.airjump = true
+                        }
+
+                        if(down == 1){
+                            this.height += (20 - this.height)/4
+                        } else {
+                            this.height += ((this.h*(3 + (Math.abs(this.velocity.y)/maxSpeed))/(3 + (Math.abs(this.velocity.x)/maxSpeed))) - this.height)/4
+                        }
+                    }
+
+                }
             }
         }
 
-        // up collision
-        if (this.position.y + this.height/2 > top) {
-            this.position.y = top - this.height/2
-            this.velocity.y = 0
-        }
+        if (count == 0) {
+            if(up && this.airjump && this.doubleTap) {
+                this.velocity.y = 40
+                this.airjump = false
+            }
 
-        // left right collisions
-        if (this.position.x - this.width/2 < l) {
-            this.position.x = l + this.width/2
-            this.velocity.x = 0
+            if(down == 1 && this.ddash == 1){
+                this.velocity.y = -200
+                this.ddash = 0
+            }
+            if (down == 0) {
+                this.ddash = 1
+            }
         }
+    }
 
-        if (this.position.x + this.width/2 > r) {
-            this.position.x = r - this.width/2
-            this.velocity.x = 0
-        }
+    drawReference(p=this, c=this.color) {
+        ctx.fillStyle = c
 
-        // collisions
-        this.updateCoordinates()
-        
-        this.draw()
+        camx += (this.velocity.x - camx)/10
+        camy += (this.velocity.y - camy)/20
+
+        ctx.fillStyle = this.color
+        ctx.fillRect(board.width/2 + this.position.x - this.width/2 - p.position.x + camx, board.height/2 - this.position.y - this.height/2 + p.position.y + camy, this.width, this.height)
     }
 }
