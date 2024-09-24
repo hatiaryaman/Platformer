9b1d1fa3c8e9d3af7dd3d9fffffc9f350d8c5027
hatiaryaman <hatiaryaman@gmail.com>
2024-09-24 09:45:21 -0500

    Update controls.js

--- controls.js
+++ controls.js
@@ -3,7 +3,9 @@ var left = 0;
 var right = 0;
 var down = 0;
 var up = 0;
-
+var editor = 0
+var b = 0
+var s = 0
 document.addEventListener("keydown", function(e) {
     if (e.key == "ArrowRight" || e.key == "d") {
         right = 1
@@ -17,6 +19,13 @@ document.addEventListener("keydown", function(e) {
     if (e.key == "ArrowUp" || e.key == "w") {
         up = 1
     }
+    if (e.key == " ") {
+        editor = (editor == 0)? 1 : 0
+    }
+    if (e.key == "Enter") {
+        s = (s == 0)? 1 : 0
+    }
+
 })
 
 document.addEventListener("keyup", function(e) {
@@ -32,4 +41,5 @@ document.addEventListener("keyup", function(e) {
     if (e.key == "ArrowUp" || e.key == "w") {
         up = 0
     }
+
 })
